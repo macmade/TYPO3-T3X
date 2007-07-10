d88cb76ce973a4bf0cadefdd07e01cf5b3135824
macmade <macmade>
2007-07-10 13:54:58 +0000

    Import of extension 'hypernav'.

---
+++ hypernav/ext_emconf.php
@@ -0,0 +1,50 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "hypernav"
+#
+# Auto generated 10-07-2007 14:58
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Hyper Navigation System',
+	'description' => 'The Hyper Navigation System plugin is a rootline type menu, where each section is a link with a drop-down menu displaying the sub pages. It can display as many sub levels as you want. This way, with a simple rootline, every single page of your website is accessible from anywhere.',
+	'category' => 'fe',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author' => 'Jean-David Gadina',
+	'author_email' => 'macmade@gadlab.net',
+	'author_company' => 'macmade.net',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '3.5.0-0.0.0',
+			'php' => '3.0.0-0.0.0',
+			'cms' => '',
+			'lang' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"499b";s:24:"ext_typoscript_setup.txt";s:4:"7f27";s:14:"doc/manual.sxw";s:4:"0ba7";s:29:"pi1/class.tx_hypernav_pi1.php";s:4:"108e";}',
+);
+
+?>
---
+++ hypernav/pi1/class.tx_hypernav_pi1.php
@@ -0,0 +1,158 @@
+<?php
+	/***************************************************************
+	 * Copyright notice
+	 *
+	 * (c) 2005 macmade.net
+	 * All rights reserved
+	 *
+	 * This script is part of the TYPO3 project. The TYPO3 project is 
+	  * free software; you can redistribute it and/or modify
+	 * it under the terms of the GNU General Public License as published by
+	 * the Free Software Foundation; either version 2 of the License, or
+	 * (at your option) any later version.
+	 *
+	 * The GNU General Public License can be found at
+	 * http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 * This script is distributed in the hope that it will be useful,
+	 * but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 * GNU General Public License for more details.
+	 *
+	 * This copyright notice MUST APPEAR in all copies of the script!
+	 ***************************************************************/
+	
+	/**
+	 * Plugin 'Hyper Navigation System' for the 'hypernav' extension.
+	 *
+	 * @author		Jean-David Gadina (macmade@gadlab.net)
+	 * @version		1.0
+	 */
+	
+	/**
+	 * [CLASS/FUNCTION INDEX OF SCRIPT]
+	 * 
+	 * SECTION:		1 - MAIN
+	 *      91:		function main($content,$conf)
+	 * 
+	 *				TOTAL FUNCTIONS: 1
+	 */
+	
+	// Typo3 menu classes
+	require_once(PATH_tslib.'media/scripts/tmenu_layers.php');
+	
+	// Typo3 FE plugin class
+	require_once(PATH_tslib.'class.tslib_pibase.php');
+	
+	class tx_hypernav_pi1 extends tslib_pibase {
+		
+		
+		
+		
+		
+		/***************************************************************
+		 * SECTION 0 - VARIABLES
+		 *
+		 * Class variables for the plugin.
+		 ***************************************************************/
+		
+		// Same as class name
+		var $prefixId = 'tx_hypernav_pi1';
+		
+		// Path to this script relative to the extension dir
+		var $scriptRelPath = 'pi1/class.tx_hypernav_pi1.php';
+		
+		// The extension key
+		var $extKey = 'hypernav';
+		
+		// Check CHash
+		var $pi_checkCHash = TRUE;
+		
+		
+		
+		
+		
+		/***************************************************************
+		 * SECTION 1 - MAIN
+		 *
+		 * Functions for the initialization and the output of the plugin.
+		 ***************************************************************/
+		
+		/**
+		 * Returns the content object of the plugin.
+		 * 
+		 * This function initialises the plugin "tx_dropdownsitemap_pi1", and
+		 * launches the needed functions to correctly display the plugin.
+		 * 
+		 * @param		$content			The content object
+		 * @param		$conf				The TS setup
+		 * @return		The content of the plugin.
+		 */
+		function main($content,$conf) {
+			
+			// Storage
+			$htmlCode = array();
+			
+			// New instance of the tslib_tmenu class
+			$menu = t3lib_div::makeInstance('tslib_tmenu_layers');
+			
+			// Menu options
+			$GLOBALS['TSFE']->register['count_HMENU']++;
+			$GLOBALS['TSFE']->register['count_HMENU_MENUOBJ'] = 0;
+			$GLOBALS['TSFE']->applicationData['GMENU_LAYERS']['WMid'] = array();
+			$GLOBALS['TSFE']->applicationData['GMENU_LAYERS']['WMparentId'] = array();
+			
+			// Set some internal vars
+			$menu->parent_cObj = $this;
+			
+			// Menu configuration
+			$mconf = array(
+				
+				// Special property
+				'special' => 'rootline',
+				
+				// Add first level
+				'1' => 'TMENU_LAYERS',
+				'1.' => $conf['firstLevel.'],
+			);
+			
+			// Force expAll property
+			$mconf['1.']['expAll'] = 1;
+			
+			// Build each level
+			for ($i = 0; $i < $conf['showLevels']; $i++) {
+				
+				// New menu object
+				$mconf[2 + $i] = 'TMENU_LAYERS';
+				
+				// Menu configuration
+				$mconf[(2 + $i) . '.'] = $conf['subLevels.'];
+				
+				// Force expAll property
+				$mconf[(2 + $i) . '.']['expAll'] = 1;
+			}
+			
+			// DEBUG ONLY - Show complete menu configuration
+			#t3lib_div::debug($mconf);
+			
+			// Class constructor
+			$menu->start($GLOBALS['TSFE']->tmpl,$GLOBALS['TSFE']->sys_page,0,$mconf,1);
+				
+			// Make the menu
+			$menu->makeMenu();
+			
+			// Write the full menu with sub-items
+			$htmlCode[] = $menu->writeMenu();
+			
+			// Return content
+			return $this->pi_wrapInBaseClass(implode(chr(10),$htmlCode));
+		}
+	}
+	
+	/**
+	 * XCLASS inclusion
+	 */
+	if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/hypernav/pi1/class.tx_hypernav_pi1.php']) {
+		include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/hypernav/pi1/class.tx_hypernav_pi1.php']);
+	}
+?>
