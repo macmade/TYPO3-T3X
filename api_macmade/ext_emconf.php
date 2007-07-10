866f2169cfb0c0f28ce64304de9220cc9a020dc7
macmade <macmade>
2007-07-10 13:49:36 +0000

    Import of extension 'api_macmade'.

---
+++ api_macmade/ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "api_macmade"
+#
+# Auto generated 10-07-2007 14:57
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Developer API',
+	'description' => 'This extension provides an API to help developing Typo3 extensions. It includes helpful functions, for frontend, backend, databases and miscellaneous development. Please take a look at the manual for a complete description of this API.',
+	'category' => 'misc',
+	'shy' => 0,
+	'version' => '0.2.8',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Jean-David Gadina',
+	'author_email' => 'info@macmade.net',
+	'author_company' => 'macmade.net',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '3.0.0-0.0.0',
+			'typo3' => '3.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:5:{s:9:"ChangeLog";s:4:"9ebb";s:7:"GPL.txt";s:4:"a073";s:23:"class.tx_apimacmade.php";s:4:"c83b";s:12:"ext_icon.gif";s:4:"c839";s:14:"doc/manual.sxw";s:4:"68ac";}',
+);
+
+?>
