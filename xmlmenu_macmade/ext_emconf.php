7e099efa73e2b49da5047307f35188d135172696
macmade <macmade>
2007-07-18 21:35:56 +0000

    Updated ext_emconf.

--- xmlmenu_macmade/ext_emconf.php
+++ xmlmenu_macmade/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "xmlmenu_macmade"
 #
-# Auto generated 09-07-2007 14:10
+# Auto generated 18-07-2007 23:35
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:31:{s:34:"class.tx_vdgeomapprototype_tca.php";s:4:"7d44";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"c482";s:14:"ext_tables.php";s:4:"8b8e";s:28:"ext_typoscript_constants.txt";s:4:"9f85";s:24:"ext_typoscript_setup.txt";s:4:"c8ab";s:19:"flexform_ds_pi1.xml";s:4:"d3be";s:13:"locallang.xml";s:4:"6798";s:16:"locallang_db.xml";s:4:"4c76";s:14:"pi1/ce_wiz.gif";s:4:"87fb";s:38:"pi1/class.tx_vdgeomapprototype_pi1.php";s:4:"1ad4";s:46:"pi1/class.tx_vdgeomapprototype_pi1_wizicon.php";s:4:"9ab7";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9b67";s:12:"res/down.gif";s:4:"0b7a";s:12:"res/help.jpg";s:4:"4e2d";s:17:"res/left-down.gif";s:4:"0385";s:15:"res/left-up.gif";s:4:"0e4b";s:12:"res/left.gif";s:4:"8e36";s:17:"res/minus-off.gif";s:4:"89cb";s:13:"res/minus.gif";s:4:"61d5";s:16:"res/plus-off.gif";s:4:"0d6d";s:12:"res/plus.gif";s:4:"6de1";s:16:"res/prototype.js";s:4:"3766";s:18:"res/right-down.gif";s:4:"10fa";s:16:"res/right-up.gif";s:4:"4c66";s:13:"res/right.gif";s:4:"a4f3";s:10:"res/up.gif";s:4:"742a";s:20:"res/zoom-current.gif";s:4:"8090";s:21:"res/zoom-decrease.gif";s:4:"d160";s:21:"res/zoom-increase.gif";s:4:"a20c";}',
+	'_md5_values_when_last_written' => 'a:8:{s:32:"class.tx_xmlmenumacmade_menu.php";s:4:"3c80";s:21:"ext_conf_template.txt";s:4:"4523";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"4c81";s:14:"ext_tables.php";s:4:"4bb2";s:14:"xml_output.php";s:4:"8a93";s:20:"static/constants.txt";s:4:"1a88";s:16:"static/setup.txt";s:4:"2fd5";}',
 	'suggests' => array(
 	),
 );
