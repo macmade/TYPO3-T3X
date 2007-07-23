3f03c41553f687ba8686ed806413921a1b001f3d
macmade <macmade>
2007-07-23 15:30:57 +0000

    Version was published on the TER.

--- xmlmenu_macmade/ext_emconf.php
+++ xmlmenu_macmade/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "xmlmenu_macmade"
 #
-# Auto generated 23-07-2007 17:12
+# Auto generated 23-07-2007 17:30
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A plugin to generate a XML menu for the TYPO3 site.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.1.0',
+	'version' => '0.1.1',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -40,7 +40,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:32:"class.tx_xmlmenumacmade_menu.php";s:4:"d588";s:21:"ext_conf_template.txt";s:4:"4523";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"41e7";s:14:"ext_tables.php";s:4:"4bb2";s:14:"xml_output.php";s:4:"8a93";s:14:"doc/manual.sxw";s:4:"db89";s:15:"res/xmlmenu.xsd";s:4:"3d9c";s:20:"static/constants.txt";s:4:"4dfd";s:16:"static/setup.txt";s:4:"fbeb";}',
+	'_md5_values_when_last_written' => 'a:10:{s:32:"class.tx_xmlmenumacmade_menu.php";s:4:"d588";s:21:"ext_conf_template.txt";s:4:"4523";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"41e7";s:14:"ext_tables.php";s:4:"4bb2";s:14:"xml_output.php";s:4:"8a93";s:14:"doc/manual.sxw";s:4:"aeb5";s:15:"res/xmlmenu.xsd";s:4:"3d9c";s:20:"static/constants.txt";s:4:"4dfd";s:16:"static/setup.txt";s:4:"fbeb";}',
 	'suggests' => array(
 	),
 );
