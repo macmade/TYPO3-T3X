9450f3a833834ffeffe3dbfa78ed09a93e542722
macmade <macmade>
2007-08-13 18:04:37 +0000

    Updated documentation and ext_emconf.

--- vd_geomap_prototype/ext_emconf.php
+++ vd_geomap_prototype/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "vd_geomap_prototype"
 #
-# Auto generated 13-08-2007 13:46
+# Auto generated 13-08-2007 20:04
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:32:{s:20:"class.ext_update.php";s:4:"deb5";s:34:"class.tx_vdgeomapprototype_tca.php";s:4:"7d44";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"c482";s:14:"ext_tables.php";s:4:"8b8e";s:28:"ext_typoscript_constants.txt";s:4:"ce78";s:24:"ext_typoscript_setup.txt";s:4:"94af";s:19:"flexform_ds_pi1.xml";s:4:"d3be";s:13:"locallang.xml";s:4:"6798";s:16:"locallang_db.xml";s:4:"4c76";s:14:"pi1/ce_wiz.gif";s:4:"87fb";s:38:"pi1/class.tx_vdgeomapprototype_pi1.php";s:4:"4952";s:46:"pi1/class.tx_vdgeomapprototype_pi1_wizicon.php";s:4:"9ab7";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9b67";s:12:"res/down.gif";s:4:"0b7a";s:12:"res/help.jpg";s:4:"4e2d";s:17:"res/left-down.gif";s:4:"0385";s:15:"res/left-up.gif";s:4:"0e4b";s:12:"res/left.gif";s:4:"8e36";s:17:"res/minus-off.gif";s:4:"89cb";s:13:"res/minus.gif";s:4:"61d5";s:16:"res/plus-off.gif";s:4:"0d6d";s:12:"res/plus.gif";s:4:"6de1";s:16:"res/prototype.js";s:4:"3766";s:18:"res/right-down.gif";s:4:"10fa";s:16:"res/right-up.gif";s:4:"4c66";s:13:"res/right.gif";s:4:"a4f3";s:10:"res/up.gif";s:4:"742a";s:20:"res/zoom-current.gif";s:4:"8090";s:21:"res/zoom-decrease.gif";s:4:"d160";s:21:"res/zoom-increase.gif";s:4:"a20c";}',
+	'_md5_values_when_last_written' => 'a:33:{s:20:"class.ext_update.php";s:4:"deb5";s:34:"class.tx_vdgeomapprototype_tca.php";s:4:"7d44";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"c482";s:14:"ext_tables.php";s:4:"8b8e";s:28:"ext_typoscript_constants.txt";s:4:"322b";s:24:"ext_typoscript_setup.txt";s:4:"0d9f";s:19:"flexform_ds_pi1.xml";s:4:"d3be";s:13:"locallang.xml";s:4:"6798";s:16:"locallang_db.xml";s:4:"4c76";s:14:"doc/manual.sxw";s:4:"622a";s:14:"pi1/ce_wiz.gif";s:4:"87fb";s:38:"pi1/class.tx_vdgeomapprototype_pi1.php";s:4:"4952";s:46:"pi1/class.tx_vdgeomapprototype_pi1_wizicon.php";s:4:"9ab7";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"9b67";s:12:"res/down.gif";s:4:"0b7a";s:12:"res/help.jpg";s:4:"4e2d";s:17:"res/left-down.gif";s:4:"0385";s:15:"res/left-up.gif";s:4:"0e4b";s:12:"res/left.gif";s:4:"8e36";s:17:"res/minus-off.gif";s:4:"89cb";s:13:"res/minus.gif";s:4:"61d5";s:16:"res/plus-off.gif";s:4:"0d6d";s:12:"res/plus.gif";s:4:"6de1";s:16:"res/prototype.js";s:4:"3766";s:18:"res/right-down.gif";s:4:"10fa";s:16:"res/right-up.gif";s:4:"4c66";s:13:"res/right.gif";s:4:"a4f3";s:10:"res/up.gif";s:4:"742a";s:20:"res/zoom-current.gif";s:4:"8090";s:21:"res/zoom-decrease.gif";s:4:"d160";s:21:"res/zoom-increase.gif";s:4:"a20c";}',
 	'suggests' => array(
 	),
 );
