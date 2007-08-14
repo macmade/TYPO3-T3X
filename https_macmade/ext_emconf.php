bc29a8871759f297e5203a1b866b3e2aa1c48d60
macmade <macmade>
2007-08-14 14:49:26 +0000

    Updated the documentation and ext_emconf.

--- https_macmade/ext_emconf.php
+++ https_macmade/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "https_macmade"
 #
-# Auto generated 11-07-2006 11:02
+# Auto generated 14-08-2007 16:49
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -32,8 +32,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '4.0-',
-			'php' => '4.1.0-',
+			'typo3' => '4.0-0.0.0',
+			'php' => '4.1.0-0.0.0',
 			'cms' => '',
 			'lang' => '',
 		),
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:9:"ChangeLog";s:4:"3ce4";s:10:"README.txt";s:4:"9fa9";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"36a5";s:14:"ext_tables.sql";s:4:"27b2";s:16:"locallang_db.xml";s:4:"2339";}',
+	'_md5_values_when_last_written' => 'a:9:{s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"91eb";s:14:"ext_tables.php";s:4:"a081";s:14:"ext_tables.sql";s:4:"27b2";s:16:"locallang_db.xml";s:4:"99ff";s:14:"doc/manual.sxw";s:4:"289e";s:33:"pi1/class.tx_httpsmacmade_pi1.php";s:4:"7ea0";s:24:"pi1/static/constants.txt";s:4:"189a";s:20:"pi1/static/setup.txt";s:4:"03ff";}',
+	'suggests' => array(
+	),
 );
 
 ?>
