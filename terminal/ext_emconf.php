6fdb5689d4b769a09483df81af2e00f0132d5218
macmade <macmade>
2007-08-15 16:18:02 +0000

    Updated the documentation and ext_emconf.

--- terminal/ext_emconf.php
+++ terminal/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "terminal"
 #
-# Auto generated 14-08-2007 09:55
+# Auto generated 15-08-2007 18:17
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:27:{s:21:"ext_conf_template.txt";s:4:"aac4";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"de54";s:14:"ext_tables.php";s:4:"b228";s:14:"doc/manual.sxw";s:4:"d7f5";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"8e83";s:14:"mod1/index.php";s:4:"bd88";s:18:"mod1/locallang.xml";s:4:"de5c";s:22:"mod1/locallang_mod.xml";s:4:"fba0";s:19:"mod1/moduleicon.gif";s:4:"c0c0";s:15:"mod1/scripts.js";s:4:"4ed4";s:14:"res/custom.png";s:4:"a28a";s:12:"res/date.png";s:4:"b139";s:10:"res/df.png";s:4:"8996";s:17:"res/fileadmin.png";s:4:"0601";s:16:"res/ifconfig.png";s:4:"33e0";s:10:"res/ls.png";s:4:"f6a8";s:11:"res/pwd.png";s:4:"3750";s:12:"res/site.png";s:4:"99be";s:13:"res/t3lib.png";s:4:"cf44";s:11:"res/top.png";s:4:"5f72";s:13:"res/typo3.png";s:4:"4866";s:17:"res/typo3conf.png";s:4:"f39d";s:17:"res/typo3temp.png";s:4:"9a18";s:15:"res/uploads.png";s:4:"b286";s:14:"res/whoami.png";s:4:"4fb4";}',
+	'_md5_values_when_last_written' => 'a:27:{s:21:"ext_conf_template.txt";s:4:"8e59";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"de54";s:14:"ext_tables.php";s:4:"b228";s:14:"doc/manual.sxw";s:4:"395e";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"8e83";s:14:"mod1/index.php";s:4:"865f";s:18:"mod1/locallang.xml";s:4:"de5c";s:22:"mod1/locallang_mod.xml";s:4:"fba0";s:19:"mod1/moduleicon.gif";s:4:"c0c0";s:15:"mod1/scripts.js";s:4:"4ed4";s:14:"res/custom.png";s:4:"a28a";s:12:"res/date.png";s:4:"b139";s:10:"res/df.png";s:4:"8996";s:17:"res/fileadmin.png";s:4:"0601";s:16:"res/ifconfig.png";s:4:"33e0";s:10:"res/ls.png";s:4:"f6a8";s:11:"res/pwd.png";s:4:"3750";s:12:"res/site.png";s:4:"99be";s:13:"res/t3lib.png";s:4:"cf44";s:11:"res/top.png";s:4:"5f72";s:13:"res/typo3.png";s:4:"4866";s:17:"res/typo3conf.png";s:4:"f39d";s:17:"res/typo3temp.png";s:4:"9a18";s:15:"res/uploads.png";s:4:"b286";s:14:"res/whoami.png";s:4:"4fb4";}',
 	'suggests' => array(
 	),
 );
