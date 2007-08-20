d6d610273f14b123912701ebdc98ce83dfa54a34
macmade <macmade>
2007-08-20 15:26:34 +0000

    Version 0.1.0 has been published to the TER.

--- tslib_patcher/ext_emconf.php
+++ tslib_patcher/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "tslib_patcher"
 #
-# Auto generated 20-08-2007 17:13
+# Auto generated 20-08-2007 17:16
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -29,7 +29,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => 'macmade.net',
-	'version' => '0.0.0',
+	'version' => '0.1.0',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '4.1.2-0.0.0',
