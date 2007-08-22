2c57b4d3a718968bddaf118bf8d606e08181f2c9
macmade <macmade>
2007-08-22 16:25:29 +0000

    Version 0.3.3 has been published to the TER.

--- api_macmade/ext_emconf.php
+++ api_macmade/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "api_macmade"
 #
-# Auto generated 22-08-2007 18:22
+# Auto generated 22-08-2007 18:25
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extension provides an API to help developing Typo3 extensions. It includes helpful functions, for frontend, backend, databases and miscellaneous development. Please take a look at the manual for a complete description of this API.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.3.2',
+	'version' => '0.3.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	),
 	'_md5_values_when_last_written' => 'a:17:{s:9:"ChangeLog";s:4:"367e";s:7:"GPL.txt";s:4:"a073";s:23:"class.tx_apimacmade.php";s:4:"dcb8";s:12:"ext_icon.gif";s:4:"c839";s:14:"doc/manual.sxw";s:4:"f457";s:29:"res/js/prototype/prototype.js";s:4:"3766";s:30:"res/js/scriptaculous/CHANGELOG";s:4:"3406";s:32:"res/js/scriptaculous/MIT-LICENSE";s:4:"9f3d";s:27:"res/js/scriptaculous/README";s:4:"5c5d";s:35:"res/js/scriptaculous/src/builder.js";s:4:"f2ab";s:36:"res/js/scriptaculous/src/controls.js";s:4:"6e5f";s:36:"res/js/scriptaculous/src/dragdrop.js";s:4:"7f11";s:35:"res/js/scriptaculous/src/effects.js";s:4:"ab48";s:41:"res/js/scriptaculous/src/scriptaculous.js";s:4:"783b";s:34:"res/js/scriptaculous/src/slider.js";s:4:"8baa";s:33:"res/js/scriptaculous/src/sound.js";s:4:"5a58";s:36:"res/js/scriptaculous/src/unittest.js";s:4:"da7d";}',
 	'suggests' => array(
-		'tslib_patcher' => '0.1.1-',
+		'tslib_patcher' => '0.1.3-',
 	),
 );
 
