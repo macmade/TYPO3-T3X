ad3d6fa1f58cea82aa62f3ae03a2f2584788313f
macmade <macmade>
2007-08-22 14:47:16 +0000

    Updated the documentation and ext_emconf. Version 0.3.5 has been published to the TER.

--- loginbox_macmade/ext_emconf.php
+++ loginbox_macmade/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "loginbox_macmade"
 #
-# Auto generated 10-10-2006 13:21
+# Auto generated 22-08-2007 16:46
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'A simple login box which allows you to define a different starting point for each instance of the plugin.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'dependencies' => 'cms,lang,api_macmade',
+	'dependencies' => 'api_macmade',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -32,13 +32,13 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'macmade.net',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '0.3.4',
-	'_md5_values_when_last_written' => 'a:22:{s:9:".DS_Store";s:4:"1efb";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"b986";s:14:"ext_tables.php";s:4:"1937";s:28:"ext_typoscript_constants.txt";s:4:"b621";s:24:"ext_typoscript_setup.txt";s:4:"4ded";s:19:"flexform_ds_pi1.xml";s:4:"2e34";s:13:"locallang.xml";s:4:"1195";s:16:"locallang_db.xml";s:4:"d060";s:13:"doc/.DS_Store";s:4:"82ad";s:14:"doc/manual.sxw";s:4:"06d7";s:14:"pi1/ce_wiz.gif";s:4:"6e35";s:36:"pi1/class.tx_loginboxmacmade_pi1.php";s:4:"d83f";s:44:"pi1/class.tx_loginboxmacmade_pi1_wizicon.php";s:4:"3408";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.php";s:4:"bb42";s:17:"pi1/locallang.xml";s:4:"cbc9";s:36:"pi2/class.tx_loginboxmacmade_pi2.php";s:4:"2da7";s:13:"pi2/clear.gif";s:4:"cc11";s:13:"res/login.gif";s:4:"5192";s:14:"res/logout.gif";s:4:"4034";s:17:"res/template.html";s:4:"c794";}',
+	'version' => '0.3.5',
+	'_md5_values_when_last_written' => 'a:19:{s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"d778";s:14:"ext_tables.php";s:4:"1370";s:28:"ext_typoscript_constants.txt";s:4:"7f06";s:24:"ext_typoscript_setup.txt";s:4:"d8e5";s:19:"flexform_ds_pi1.xml";s:4:"46ff";s:13:"locallang.xml";s:4:"3c1c";s:16:"locallang_db.xml";s:4:"c189";s:14:"doc/manual.sxw";s:4:"08a9";s:14:"pi1/ce_wiz.gif";s:4:"6e35";s:36:"pi1/class.tx_loginboxmacmade_pi1.php";s:4:"2df8";s:44:"pi1/class.tx_loginboxmacmade_pi1_wizicon.php";s:4:"d656";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"502b";s:36:"pi2/class.tx_loginboxmacmade_pi2.php";s:4:"6aa6";s:13:"pi2/clear.gif";s:4:"cc11";s:13:"res/login.gif";s:4:"5192";s:14:"res/logout.gif";s:4:"4034";s:17:"res/template.html";s:4:"75ba";}',
 	'constraints' => array(
 		'depends' => array(
 			'api_macmade' => '0.3.2-',
-			'php' => '3.0.0-',
-			'typo3' => '3.5.0-',
+			'php' => '3.0.0-0.0.0',
+			'typo3' => '3.5.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
