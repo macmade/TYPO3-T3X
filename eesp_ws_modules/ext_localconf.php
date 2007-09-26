85f25b9af29a8f7036d01be93db6dd287505afad
macmade <macmade>
2007-09-26 11:51:11 +0000

    Import of base extension files.

---
+++ eesp_ws_modules/ext_localconf.php
@@ -0,0 +1,14 @@
+<?php
+if( !defined( 'TYPO3_MODE' ) ) {
+    die ( 'Access denied.' );
+}
+
+// Add FE plugin
+t3lib_extMgm::addPItoST43(
+    $_EXTKEY,
+    'pi1/class.tx_eespwsmodules_pi1.php',
+    '_pi1',
+    'list_type',
+    0
+);
+?>
