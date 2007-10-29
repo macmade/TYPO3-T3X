6b94db70b945b6ca9a2e12dba2ca01f72c4864c0
macmade <macmade>
2007-10-29 14:32:33 +0000

    Version 0.4.2 has been published to the TER.

--- api_macmade/ext_emconf.php
+++ api_macmade/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "api_macmade"
 #
-# Auto generated 29-10-2007 15:30
+# Auto generated 29-10-2007 15:32
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'This extension provides an API to help developing Typo3 extensions. It includes helpful functions, for frontend, backend, databases and miscellaneous development. Please take a look at the manual for a complete description of this API.',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '0.4.1',
+	'version' => '0.4.2',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
