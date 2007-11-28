f7fe9919d5e6c38fa2a197fb64a7d5b37c98c36f
macmade <macmade>
2007-11-28 00:39:14 +0000

    Updated ext_emconf.

--- dropdown_sitemap/ext_emconf.php
+++ dropdown_sitemap/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "dropdown_sitemap"
 #
-# Auto generated 15-08-2007 18:01
+# Auto generated 28-11-2007 01:38
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'The Drop-Down sitemap plugin adds a new kind of menu/sitemap to the TYPO3 content elements. It uses HTML list elements, CSS and JavaScript (Scriptaculous if desired) to generate a drop-down map of the website, with the possibility to expand / collapse every section.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'dependencies' => 'cms,lang,api_macmade',
+	'dependencies' => 'api_macmade',
 	'conflicts' => '',
 	'priority' => '',
 	'loadOrder' => '',
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '1.5.0',
-	'_md5_values_when_last_written' => 'a:17:{s:20:"class.ext_update.php";s:4:"6ed8";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"a2f3";s:14:"ext_tables.php";s:4:"dd39";s:28:"ext_typoscript_constants.txt";s:4:"7804";s:24:"ext_typoscript_setup.txt";s:4:"acfa";s:19:"flexform_ds_pi1.xml";s:4:"bab5";s:13:"locallang.xml";s:4:"3723";s:16:"locallang_db.xml";s:4:"cf9a";s:14:"doc/manual.sxw";s:4:"777a";s:14:"pi1/ce_wiz.gif";s:4:"1ea2";s:36:"pi1/class.tx_dropdownsitemap_pi1.php";s:4:"02aa";s:44:"pi1/class.tx_dropdownsitemap_pi1_wizicon.php";s:4:"3f5b";s:17:"pi1/locallang.xml";s:4:"ee9a";s:13:"pi1/minus.gif";s:4:"833d";s:12:"pi1/plus.gif";s:4:"3c64";s:14:"pi1/spacer.gif";s:4:"3254";}',
+	'_md5_values_when_last_written' => 'a:21:{s:20:"class.ext_update.php";s:4:"6ed8";s:32:"class.tx_dropdownsitemap_tca.php";s:4:"420c";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"a2f3";s:14:"ext_tables.php";s:4:"65f4";s:19:"flexform_ds_pi1.xml";s:4:"6fbf";s:13:"locallang.xml";s:4:"3723";s:16:"locallang_db.xml";s:4:"790e";s:14:"doc/manual.sxw";s:4:"777a";s:14:"pi1/ce_wiz.gif";s:4:"1ea2";s:36:"pi1/class.tx_dropdownsitemap_pi1.php";s:4:"1780";s:44:"pi1/class.tx_dropdownsitemap_pi1_wizicon.php";s:4:"3f5b";s:17:"pi1/locallang.xml";s:4:"9150";s:15:"res/exp-off.gif";s:4:"8587";s:14:"res/exp-on.gif";s:4:"6def";s:18:"res/folder-off.gif";s:4:"8c47";s:17:"res/folder-on.gif";s:4:"4431";s:12:"res/page.gif";s:4:"a659";s:14:"res/spacer.gif";s:4:"2a7d";s:23:"static/ts/constants.txt";s:4:"a2ad";s:19:"static/ts/setup.txt";s:4:"d8b4";}',
 	'constraints' => array(
 		'depends' => array(
 			'api_macmade' => '0.4.2-',
