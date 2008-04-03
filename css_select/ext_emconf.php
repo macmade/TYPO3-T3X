7182c3286b8bbb51927e633b85a784768a09649c
macmade <macmade>
2008-04-03 22:52:32 +0000

    Updated ext_emconf.

--- css_select/ext_emconf.php
+++ css_select/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "css_select"
 #
-# Auto generated 10-07-2007 14:58
+# Auto generated 04-04-2008 00:52
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -34,14 +34,16 @@ $EM_CONF[$_EXTKEY] = array(
 	'version' => '0.5.0',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.2.0-'
+			'php' => '5.2.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:40:"class.tx_cssselect_handlestylesheets.php";s:4:"dbc3";s:21:"ext_conf_template.txt";s:4:"6510";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"8b03";s:14:"ext_tables.php";s:4:"9059";s:14:"ext_tables.sql";s:4:"5b95";s:28:"ext_typoscript_constants.txt";s:4:"14b8";s:24:"ext_typoscript_setup.txt";s:4:"8324";s:16:"locallang_db.php";s:4:"9cf0";s:14:"doc/manual.sxw";s:4:"3a83";s:30:"pi1/class.tx_cssselect_pi1.php";s:4:"555d";}',
+	'_md5_values_when_last_written' => 'a:13:{s:20:"class.ext_update.php";s:4:"8642";s:40:"class.tx_cssselect_handlestylesheets.php";s:4:"380a";s:21:"ext_conf_template.txt";s:4:"1a18";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"444a";s:14:"ext_tables.php";s:4:"3ac4";s:14:"ext_tables.sql";s:4:"e549";s:16:"locallang_db.xml";s:4:"aa7e";s:14:"doc/manual.sxw";s:4:"0ce2";s:30:"pi1/class.tx_cssselect_pi1.php";s:4:"fb15";s:11:"res/css.gif";s:4:"afab";s:20:"static/constants.txt";s:4:"1311";s:16:"static/setup.txt";s:4:"b59b";}',
+	'suggests' => array(
+	),
 );
 
 ?>
