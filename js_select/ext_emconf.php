012329932d28b3d12c58ff671dc1b230d1a86d9a
macmade <macmade>
2008-04-05 15:45:20 +0000

    Changed the extension requirements.

--- js_select/ext_emconf.php
+++ js_select/ext_emconf.php
@@ -27,24 +27,23 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author' => 'Jean-David Gadina',
-	'author_email' => 'macmade@gadlab.net',
+	'author_email' => 'info@macmade.net',
 	'author_company' => 'macmade.net',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
 	'version' => '0.3.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '3.5.0-0.0.0',
-			'php' => '3.0.0-0.0.0',
-			'cms' => '',
-			'lang' => '',
+			'php' => '5.2.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:11:{s:39:"class.tx_jsselect_handlejavascripts.php";s:4:"8c51";s:21:"ext_conf_template.txt";s:4:"64aa";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"5e0a";s:14:"ext_tables.php";s:4:"c945";s:14:"ext_tables.sql";s:4:"8678";s:28:"ext_typoscript_constants.txt";s:4:"a937";s:24:"ext_typoscript_setup.txt";s:4:"a9c1";s:16:"locallang_db.php";s:4:"9bdd";s:14:"doc/manual.sxw";s:4:"0b54";s:29:"pi1/class.tx_jsselect_pi1.php";s:4:"8814";}',
+	'_md5_values_when_last_written' => 'a:13:{s:20:"class.ext_update.php";s:4:"8642";s:40:"class.tx_cssselect_handlestylesheets.php";s:4:"380a";s:21:"ext_conf_template.txt";s:4:"1a18";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"444a";s:14:"ext_tables.php";s:4:"3ac4";s:14:"ext_tables.sql";s:4:"e549";s:16:"locallang_db.xml";s:4:"aa7e";s:14:"doc/manual.sxw";s:4:"0ce2";s:30:"pi1/class.tx_cssselect_pi1.php";s:4:"2ca5";s:11:"res/css.gif";s:4:"afab";s:20:"static/constants.txt";s:4:"1311";s:16:"static/setup.txt";s:4:"b59b";}',
+	'suggests' => array(
+	),
 );
 
 ?>
