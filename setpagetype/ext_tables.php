867ab3ea149db030aa3e0d115b341cd1a0fdbfaf
macmade <macmade>
2008-04-07 22:07:42 +0000

    Code cleaning.

--- setpagetype/ext_tables.php
+++ setpagetype/ext_tables.php
@@ -1,14 +1,16 @@
 <?php
-	if (!defined ('TYPO3_MODE')) {
-		die ('Access denied.');
-	}
-	
-	if (TYPO3_MODE == 'BE') {
-		
-		// Add CSM item
-		$GLOBALS['TBE_MODULES_EXT']['xMOD_alt_clickmenu']['extendCMclasses'][] = array(
-			'name' => 'tx_setpagetype_cm1',
-			'path' => t3lib_extMgm::extPath($_EXTKEY) . 'class.tx_setpagetype_cm1.php'
-		);
-	}
+if( !defined( 'TYPO3_MODE' ) ) {
+    
+    die( 'Access denied.' );
+}
+
+// Checks the TYPO3 mode
+if( TYPO3_MODE == 'BE' ) {
+    
+    // Adds the CSM item
+    $GLOBALS[ 'TBE_MODULES_EXT' ][ 'xMOD_alt_clickmenu' ][ 'extendCMclasses' ][] = array(
+        'name' => 'tx_setpagetype_cm1',
+        'path' => t3lib_extMgm::extPath( $_EXTKEY ) . 'class.tx_setpagetype_cm1.php'
+    );
+}
 ?>
