f5dcea177c4538bc896a459cb86ce520244439e0
macmade <macmade>
2008-04-07 14:11:20 +0000

    Updated ext_emconf.

--- js_select/ext_emconf.php
+++ js_select/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "js_select"
 #
-# Auto generated 10-07-2007 14:58
+# Auto generated 07-04-2008 16:10
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:20:"class.ext_update.php";s:4:"8642";s:40:"class.tx_cssselect_handlestylesheets.php";s:4:"380a";s:21:"ext_conf_template.txt";s:4:"1a18";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"444a";s:14:"ext_tables.php";s:4:"3ac4";s:14:"ext_tables.sql";s:4:"e549";s:16:"locallang_db.xml";s:4:"aa7e";s:14:"doc/manual.sxw";s:4:"0ce2";s:30:"pi1/class.tx_cssselect_pi1.php";s:4:"2ca5";s:11:"res/css.gif";s:4:"afab";s:20:"static/constants.txt";s:4:"1311";s:16:"static/setup.txt";s:4:"b59b";}',
+	'_md5_values_when_last_written' => 'a:13:{s:20:"class.ext_update.php";s:4:"aad2";s:39:"class.tx_jsselect_handlejavascripts.php";s:4:"18aa";s:21:"ext_conf_template.txt";s:4:"41c4";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"4983";s:14:"ext_tables.php";s:4:"4c35";s:14:"ext_tables.sql";s:4:"bce4";s:16:"locallang_db.xml";s:4:"8173";s:14:"doc/manual.sxw";s:4:"8662";s:29:"pi1/class.tx_jsselect_pi1.php";s:4:"517b";s:10:"res/js.gif";s:4:"f60b";s:20:"static/constants.txt";s:4:"ba0d";s:16:"static/setup.txt";s:4:"d46e";}',
 	'suggests' => array(
 	),
 );
