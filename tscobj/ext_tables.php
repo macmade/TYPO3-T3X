f1da505389179f531ab0ba64de39e6d2ac477bfd
macmade <macmade>
2008-04-08 01:05:26 +0000

    Code cleaning.

--- tscobj/ext_tables.php
+++ tscobj/ext_tables.php
@@ -1,25 +1,37 @@
 <?php
-	if (!defined ('TYPO3_MODE')) {
-		die ('Access denied.');
-	}
-	
-	// Load content TCA
-	t3lib_div::loadTCA('tt_content');
-	
-	// Plugin options
-	$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY.'_pi1']='layout,select_key,pages,recursive';
-	
-	// Add flexform fields to plugin options
-	$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY.'_pi1']='pi_flexform';
-	
-	// Add flexform DataStructures
-	t3lib_extMgm::addPiFlexFormValue($_EXTKEY.'_pi1', 'FILE:EXT:' . $_EXTKEY . '/flexform_ds_pi1.xml');
-	
-	// Add plugins
-	t3lib_extMgm::addPlugin(Array('LLL:EXT:tscobj/locallang_db.php:tt_content.list_type_pi1', $_EXTKEY.'_pi1'),'list_type');
-	
-	// Wizard icons
-	if (TYPO3_MODE=='BE') {
-		$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['tx_tscobj_pi1_wizicon'] = t3lib_extMgm::extPath($_EXTKEY).'pi1/class.tx_tscobj_pi1_wizicon.php';
-	}
+if( !defined( 'TYPO3_MODE' ) ) {
+    die( 'Access denied.' );
+}
+
+// Loads the content TCA
+t3lib_div::loadTCA( 'tt_content' );
+
+// Plugin options
+$TCA[ 'tt_content' ][ 'types' ][ 'list' ][ 'subtypes_excludelist' ][ $_EXTKEY . '_pi1' ] = 'layout,select_key,pages,recursive';
+
+// Adds the flexform fields to the plugin options
+$TCA[ 'tt_content' ][ 'types' ][ 'list' ][ 'subtypes_addlist' ][ $_EXTKEY . '_pi1' ]     = 'pi_flexform';
+
+// Adds the flexform data structure
+t3lib_extMgm::addPiFlexFormValue(
+    $_EXTKEY . '_pi1',
+    'FILE:EXT:' . $_EXTKEY . '/flexform_ds_pi1.xml'
+);
+
+// Adds the FE plugin
+t3lib_extMgm::addPlugin(
+    array(
+        'LLL:EXT:' . $_EXTKEY . '/locallang_db.php:tt_content.list_type_pi1',
+        $_EXTKEY . '_pi1'
+    ),
+    'list_type'
+);
+
+// Checks the TYPO3 mode
+if( TYPO3_MODE == 'BE' ) {
+    
+    // Adds the CE wizard icon
+    $TBE_MODULES_EXT[ 'xMOD_db_new_content_el' ][ 'addElClasses' ][ 'tx_tscobj_pi1_wizicon' ] = t3lib_extMgm::extPath( $_EXTKEY )
+                                                                                              . 'pi1/class.tx_tscobj_pi1_wizicon.php';
+}
 ?>
