6cc50aad2f2da2e86211ca938e24856ce2a71086
macmade <macmade>
2008-04-28 13:22:35 +0000

    PHP5 version of the wizard icon class.

--- dropdown_sitemap/pi1/class.tx_dropdownsitemap_pi1_wizicon.php
+++ dropdown_sitemap/pi1/class.tx_dropdownsitemap_pi1_wizicon.php
@@ -1,101 +1,117 @@
 <?php
 /***************************************************************
-* Copyright notice
-* 
-* (c) 2004 Jean-David Gadina (info@macmade.net)
-* All rights reserved
-* 
-* This script is part of the TYPO3 project. The TYPO3 project is 
-* free software; you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation; either version 2 of the License, or
-* (at your option) any later version.
-* 
-* The GNU General Public License can be found at
-* http://www.gnu.org/copyleft/gpl.html.
-* 
-* This script is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU General Public License for more details.
-* 
-* This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ * Copyright notice
+ * 
+ * (c) 2004 macmade.net
+ * All rights reserved
+ * 
+ * This script is part of the TYPO3 project. The TYPO3 project is 
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * 
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 /** 
  * Class that adds the wizard icon.
  *
  * @author		Jean-David Gadina (info@macmade.net)
- * @version		1.0
+ * @version		2.0
  */
 
 /**
  * [CLASS/FUNCTION INDEX OF SCRIPT]
  * 
- * SECTION:     1 - MAIN
- *     61:      function proc($wizardItems)
- *     93:      function includeLocalLang
+ *   43:		class tx_dropdownsitemap_pi1_wizicon
+ *   62:		public function __construct
+ *   84:		protected function _getLabel( $label )
+ *   95:		public function proc( array $wizardItems )
  * 
- *              TOTAL FUNCTIONS: 2
+ *				TOTAL FUNCTIONS: 3
  */
 
 class tx_dropdownsitemap_pi1_wizicon
 {
+    // Extension key
+    protected static $_extKey = 'dropdown_sitemap';
     
+    // Plugin name
+    protected static $_piName = 'dropdownsitemap';
     
+    // Language object
+    protected static $_lang   = NULL;
     
+    // Language labels
+    protected static $_labels = array();
     
-    
-    /***************************************************************
-     * SECTION 1 - MAIN
-     *
-     * Wizard items functions.
-     ***************************************************************/
-     
     /**
-     * Add wizard item to the backend
+     * Class constructor
      * 
-     * @param       $wizardItems        The wizard items
-     * @return      The wizard item
+     * @return  NULL
      */
-    function proc( $wizardItems )
+    public function __construct()
     {
-        global $LANG;
-        
-        // Get locallang values
-        $LL = $this->includeLocalLang();
-        
-        // Wizard item
-        $wizardItems[ 'plugins_tx_dropdownsitemap_pi1' ] = array(
+        // Checks for the language object
+        if( !is_object( self::$_lang ) ) {
             
-            // Icon
-            'icon'        => t3lib_extMgm::extRelPath( 'dropdown_sitemap' ) . 'pi1/ce_wiz.gif',
+            // Gets the language object
+            self::$_lang   = $GLOBALS[ 'LANG' ];
             
-            // Title
-            'title'       => $LANG->getLLL( 'pi1_title', $LL ),
-            
-            // Description
-            'description' => $LANG->getLLL( 'pi1_plus_wiz_description', $LL ),
-            
-            // Parameters
-            'params'      => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=dropdown_sitemap_pi1'
-        );
-        
-        // Return items
-        return $wizardItems;
+            // Gets the language labels
+            self::$_labels = self::$_lang->includeLLFile(
+                'EXT:' . self::$_extKey . '/locallang.xml',
+                false
+            );
+        }
     }
     
     /**
-     * Include locallang values
+     * Gets a locallang label
      * 
-     * @return      The content of the locallang file
+     * @param   string  $label  The name of the label to get
+     * @return  string  The locallang label
      */
-    function includeLocalLang()
+    protected function _getLabel( $label )
     {
-        $llFile    = t3lib_extMgm::extPath( 'dropdown_sitemap' ) . 'locallang.xml';
-        $localLang = t3lib_div::readLLXMLfile( $llFile, $GLOBALS[ 'LANG' ]->lang );
+        return self::$_lang->getLLL( $label, self::$_labels );
+    }
+    
+    /**
+     * Process the wizard items array
+     *
+     * @param   array   $wizardItems    The wizard items
+     * @return  array   The modified array with the wizard items
+     */
+    public function proc( array $wizardItems )
+    {
+        // Wizard item
+        $wizardItems[ 'plugins_' . self::$_piName . '_pi1' ] = array(
+            
+            // Icon
+            'icon'        => t3lib_extMgm::extRelPath( self::$_extKey ) . 'pi1/ce_wiz.gif',
+            
+            // Title
+            'title'       => $this->_getLabel( 'pi1_title' ),
+            
+            // Description
+            'description' => $this->_getLabel( 'pi1_plus_wiz_description' ),
+            
+            // Parameters
+            'params'      => '&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=' . self::$_extKey . '_pi1'
+        );
         
-        return $localLang;
+        // Returns the wizard items
+        return $wizardItems;
     }
 }
 
@@ -105,4 +121,3 @@ class tx_dropdownsitemap_pi1_wizicon
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dropdown_sitemap/pi1/class.tx_dropdownsitemap_pi1_wizicon.php']) {
     include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/dropdown_sitemap/pi1/class.tx_dropdownsitemap_pi1_wizicon.php']);
 }
-?>
