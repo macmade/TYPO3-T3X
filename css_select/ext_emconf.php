585cdf59400941e4f555fa7c38c89c762a4feae7
macmade <macmade>
2008-05-02 07:38:26 +0000

    Version 1.0.3 has been published to the TER.

--- css_select/ext_emconf.php
+++ css_select/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "css_select"
 #
-# Auto generated 07-04-2008 15:09
+# Auto generated 02-05-2008 09:38
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => 'macmade.net',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'version' => '1.0.2',
+	'version' => '1.0.3',
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
@@ -41,7 +41,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:13:{s:20:"class.ext_update.php";s:4:"8642";s:40:"class.tx_cssselect_handlestylesheets.php";s:4:"9645";s:21:"ext_conf_template.txt";s:4:"1a18";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"444a";s:14:"ext_tables.php";s:4:"3ac4";s:14:"ext_tables.sql";s:4:"e549";s:16:"locallang_db.xml";s:4:"aa7e";s:14:"doc/manual.sxw";s:4:"eb67";s:30:"pi1/class.tx_cssselect_pi1.php";s:4:"22f9";s:11:"res/css.gif";s:4:"afab";s:20:"static/constants.txt";s:4:"1311";s:16:"static/setup.txt";s:4:"b59b";}',
+	'_md5_values_when_last_written' => 'a:13:{s:20:"class.ext_update.php";s:4:"8642";s:40:"class.tx_cssselect_handlestylesheets.php";s:4:"9645";s:21:"ext_conf_template.txt";s:4:"1a18";s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"444a";s:14:"ext_tables.php";s:4:"3ac4";s:14:"ext_tables.sql";s:4:"e549";s:16:"locallang_db.xml";s:4:"aa7e";s:14:"doc/manual.sxw";s:4:"0bc2";s:30:"pi1/class.tx_cssselect_pi1.php";s:4:"667c";s:11:"res/css.gif";s:4:"afab";s:20:"static/constants.txt";s:4:"1311";s:16:"static/setup.txt";s:4:"b59b";}',
 	'suggests' => array(
 	),
 );
