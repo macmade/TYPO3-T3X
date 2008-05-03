9b1eaa0ce7598538eb8f2ffad2939f2b378f8a7b
macmade <macmade>
2008-05-03 15:40:24 +0000

    Updated ext_emconf.

--- eesp_ws_modules/ext_emconf.php
+++ eesp_ws_modules/ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext: "eesp_ws_modules"
 #
-# Auto generated 17-04-2007 11:17
+# Auto generated 03-05-2008 17:40
 #
 # Manual updates:
 # Only the data in the array - anything else is removed by next write.
@@ -42,7 +42,9 @@ $EM_CONF[$_EXTKEY] = array(
 			'tslib_patcher' => '0.1.3-',
 		),
 	),
-	'_md5_values_when_last_written' => 'a:10:{s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"d146";s:19:"doc/wizard_form.dat";s:4:"7db6";s:20:"doc/wizard_form.html";s:4:"150a";s:14:"mod1/clear.gif";s:4:"cc11";s:13:"mod1/conf.php";s:4:"2e13";s:14:"mod1/index.php";s:4:"35c4";s:18:"mod1/locallang.xml";s:4:"cc72";s:22:"mod1/locallang_mod.xml";s:4:"0a6e";s:19:"mod1/moduleicon.gif";s:4:"8074";}',
+	'_md5_values_when_last_written' => 'a:24:{s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"b4db";s:14:"ext_tables.php";s:4:"4f9c";s:19:"flexform_ds_pi1.xml";s:4:"57d2";s:13:"locallang.xml";s:4:"6581";s:16:"locallang_db.xml";s:4:"3359";s:45:"classes/class.tx_eespwsmodules_listgetter.php";s:4:"4559";s:47:"classes/class.tx_eespwsmodules_peoplegetter.php";s:4:"6171";s:47:"classes/class.tx_eespwsmodules_singlegetter.php";s:4:"19dc";s:38:"classes/class.tx_eespwsmodules_tca.php";s:4:"b665";s:14:"pi1/ce_wiz.gif";s:4:"380f";s:34:"pi1/class.tx_eespwsmodules_pi1.php";s:4:"e0cb";s:42:"pi1/class.tx_eespwsmodules_pi1_wizicon.php";s:4:"67e5";s:13:"pi1/clear.gif";s:4:"cc11";s:17:"pi1/locallang.xml";s:4:"a211";s:18:"res/classrooms.png";s:4:"e20b";s:20:"res/collaborator.png";s:4:"4fb4";s:18:"res/headers-bg.gif";s:4:"a67e";s:19:"res/information.png";s:4:"3750";s:19:"res/modinfos-bg.gif";s:4:"713a";s:11:"res/pdf.png";s:4:"5ee1";s:15:"res/student.png";s:4:"8d72";s:21:"res/template_pi1.html";s:4:"a05d";s:19:"static/ts/setup.txt";s:4:"da67";}',
+	'suggests' => array(
+	),
 );
 
 ?>
