dedcfef73f2bc23b47d2b5a14146d694d07ee6ba
macmade <macmade>
2008-06-25 16:02:21 +0000

    Initial import of base files for extension 'adler_contest'.

---
+++ adler_contest/ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "adler_contest"
+#
+# Auto generated 25-06-2008 18:01
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Adler / Contest',
+	'description' => '',
+	'category' => 'plugin',
+	'shy' => 0,
+	'dependencies' => 'api_macmade',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'internal' => 0,
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author' => 'Jean-David Gadina',
+	'author_email' => 'info@macmade.net',
+	'author_company' => 'macmade.net',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'version' => '0.0.0',
+	'_md5_values_when_last_written' => 'a:0:{}',
+	'constraints' => array(
+		'depends' => array(
+			'api_macmade' => '0.4.4-',
+			'php' => '5.2.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+);
+
+?>
