307af192972382c474b0c71214185b219d1c0f50
macmade <macmade>
2008-06-30 12:54:10 +0000

    Added the extension local configuration.

---
+++ adler_contest/ext_localconf.php
@@ -0,0 +1,29 @@
+<?php
+if( !defined( 'TYPO3_MODE' ) ) {
+    die( 'Access denied.' );
+}
+
+// FE plugins
+t3lib_extMgm::addPItoST43(
+    $_EXTKEY,
+    'pi1/class.tx_adlercontest_pi1.php',
+    '_pi1',
+    'list_type',
+    1
+);
+t3lib_extMgm::addPItoST43(
+    $_EXTKEY,
+    'pi2/class.tx_adlercontest_pi2.php',
+    '_pi2',
+    'list_type',
+    1
+);
+t3lib_extMgm::addPItoST43(
+    $_EXTKEY,
+    'pi3/class.tx_adlercontest_pi3.php',
+    '_pi3',
+    'list_type',
+    1
+);
+
+?>
