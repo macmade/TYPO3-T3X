5864ad2453f9dec1515a4e4fe8377efdfa40acbc
macmade <macmade>
2008-08-22 09:54:15 +0000

    Added methods to create a date field.

--- adler_contest/classes/class.tx_adlercontest_methodprovider.php
+++ adler_contest/classes/class.tx_adlercontest_methodprovider.php
@@ -52,6 +52,21 @@ class tx_adlercontest_methodProvider
     protected $_countriesOptions = '';
     
     /**
+     * Option tags for the days
+     */
+    protected $_daysOptions      = '';
+    
+    /**
+     * Option tags for the monthes
+     */
+    protected $_monthesOptions   = '';
+    
+    /**
+     * Option tags for the years
+     */
+    protected $_yearsOptions     = '';
+    
+    /**
      * 
      */
     protected function __construct()
@@ -120,10 +135,10 @@ class tx_adlercontest_methodProvider
                 
                 // Adds the option tag
                 $options[] = '<option value="'
-                          . $country[ 'uid' ]
-                          . '">'
-                          . $country[ 'cn_short_en' ]
-                          . '</option>';
+                           . $country[ 'uid' ]
+                           . '">'
+                           . $country[ 'cn_short_en' ]
+                           . '</option>';
             }
             
             // Stores the coutries options
@@ -137,19 +152,146 @@ class tx_adlercontest_methodProvider
     /**
      * 
      */
-    public function _countrySelect( $name )
+    protected function _getDaysOptions()
+    {
+        // Checks if the days options already exist
+        if( !$this->_daysOptions ) {
+            
+            // Storage
+            $options = array();
+            
+            // Process each day
+            for( $i = 1; $i < 32; $i++ ) {
+                
+                // Adds the option tag
+                $options[] = '<option value="'
+                           . $i
+                           . '">'
+                           . $i
+                           . '</option>';
+            }
+            
+            // Stores the days options
+            $this->_daysOptions = implode( $this->_NL, $options );
+        }
+        
+        // Returns the option tags
+        return $this->_daysOptions;
+    }
+    
+    /**
+     * 
+     */
+    protected function _getMonthesOptions()
+    {
+        // Checks if the monthes options already exist
+        if( !$this->_monthesOptions ) {
+            
+            // Storage
+            $options = array();
+            
+            // Process each month
+            for( $i = 1; $i < 13; $i++ ) {
+                
+                // Month name
+                $monthName = strftime( '%B', strtotime( '2008-' . $i . '-1' ) );
+                
+                // Adds the option tag
+                $options[] = '<option value="'
+                           . $i
+                           . '">'
+                           . $monthName
+                           . '</option>';
+            }
+            
+            // Stores the monthes options
+            $this->_monthesOptions = implode( $this->_NL, $options );
+        }
+        
+        // Returns the option tags
+        return $this->_monthesOptions;
+    }
+    
+    /**
+     * 
+     */
+    protected function _getYearsOptions()
+    {
+        // Checks if the years options already exist
+        if( !$this->_yearsOptions ) {
+            
+            // Storage
+            $options     = array();
+            
+            // Current year
+            $currentYear = date( 'Y' );
+            
+            // Lowest year
+            $lowYear     = $currentYear - 101;
+            
+            // Process each year
+            for( $i = $currentYear; $i > $lowYear; $i-- ) {
+                
+                // Adds the option tag
+                $options[] = '<option value="'
+                           . $i
+                           . '">'
+                           . $i
+                           . '</option>';
+            }
+            
+            // Stores the years options
+            $this->_yearsOptions = implode( $this->_NL, $options );
+        }
+        
+        // Returns the option tags
+        return $this->_yearsOptions;
+    }
+    
+    /**
+     * 
+     */
+    public function countrySelect( $name )
     {
         // Select code
         $select = '<select name="'
-                . $this->prefixId
-                . '['
                 . $name
-                . ']" size="1">'
+                . '" size="1">'
                 . $this->_getCountriesOptions()
                 . '</select>';
         
         // Returns the select box
         return $select;
+    }
+    
+    /**
+     * 
+     */
+    public function dateSelect( $name )
+    {
+        // Select code for the days
+        $selectDays    = '<select name="'
+                       . $name
+                       . '[day]" size="1">'
+                       . $this->_getDaysOptions()
+                       . '</select>';
+        
+        // Select code for the days
+        $selectMonthes = '<select name="'
+                       . $name
+                       . '[month]" size="1">'
+                       . $this->_getMonthesOptions()
+                       . '</select>';
+        
+        // Select code for the days
+        $selectYears   = '<select name="'
+                       . $name
+                       . '[year]" size="1">'
+                       . $this->_getYearsOptions()
+                       . '</select>';
+        
+        // Returns the select box
+        return $selectDays . ' ' . $selectMonthes . ' ' . $selectYears;
     }
 }
 
