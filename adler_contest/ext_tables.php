9aa70ff55a6e92cbd2bcfd96b2d5aa89d1422ee4
macmade <macmade>
2008-08-23 15:43:56 +0000

    Added the inclusion of the backend module.

--- adler_contest/ext_tables.php
+++ adler_contest/ext_tables.php
@@ -3,6 +3,18 @@ if( !defined( 'TYPO3_MODE' ) ) {
     die( 'Access denied.' );
 }
 
+// Checks the TYPO3 mode
+if( TYPO3_MODE === 'BE' ) {
+    
+    // Adds the backend module
+    t3lib_extMgm::addModule(
+        'web',
+        'txtcatunerM1',
+        '',
+        t3lib_extMgm::extPath( $_EXTKEY ) . 'mod1/'
+    );
+}
+
 // Prefix for the frontend plugins
 $tempPiPrefix = str_replace( '_', '', $_EXTKEY );
 
