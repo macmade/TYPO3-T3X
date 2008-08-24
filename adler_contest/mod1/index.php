fe47f651491bb18f21d4cecda4d8a149d81150d7
macmade <macmade>
2008-08-24 10:24:47 +0000

    Added basic user listing.

--- adler_contest/mod1/index.php
+++ adler_contest/mod1/index.php
@@ -86,6 +86,14 @@ class tx_adlercontest_module1 extends t3lib_SCbase
     protected static $_client        = array();
     
     /**
+     * The database tables used by this extension
+     */
+    protected static $_dbTables      = array(
+        'users'    => 'fe_users',
+        'profiles' => 'tx_adlercontest_users'
+    );
+    
+    /**
      * The back path (to the TYPO3 directory)
      */
     protected static $_backPath      = '';
@@ -94,6 +102,11 @@ class tx_adlercontest_module1 extends t3lib_SCbase
      * The new line character
      */
     protected static $_NL            = '';
+    
+    /**
+     * The date format
+     */
+    protected static $_dateFormat    = 'd.m.Y / H:i';
     
     /**
      * The instance of the Developer API
@@ -106,14 +119,29 @@ class tx_adlercontest_module1 extends t3lib_SCbase
     protected $_page                 = array();
     
     /**
-     * The module content
+     * The page informations
      */
     protected $_pageInfos            = array();
     
     /**
-     * 
+     * The module content
      */
     protected $_content              = array();
+    
+    /**
+     * 
+     */
+    protected $_users                = array();
+    
+    /**
+     * 
+     */
+    protected $_profiles             = array();
+    
+    /**
+     * 
+     */
+    protected $_startedTags          = array();
     
     /**
      * 
@@ -232,9 +260,320 @@ class tx_adlercontest_module1 extends t3lib_SCbase
     /**
      * 
      */
+    protected function _tag( $tag, $content = '', array $params = array(), array $styles = array(), $onlyStart = false )
+    {
+        // Storage
+        $tagParams    = '';
+        $inlineStyles = '';
+        
+        // Process each parameter
+        foreach( $params as $key => $value ) {
+            
+            // The style parameter is not allowed
+            if( $key === 'style' ) {
+                
+                // Process the next parameter
+                continue;
+            }
+            
+            // Adds the parameter
+            $tagParams .= ' ' . $key . '="' . $value . '"';
+        }
+        
+        // Checks for styles
+        if( count( $styles ) ) {
+            
+            // Adds the style parameter
+            $inlineStyles .= ' style="';
+            
+            // Process each style
+            foreach( $styles as $key => $value ) {
+                
+                // Adds the style
+                $inlineStyles .= $key . ': ' . $value . ';';
+            }
+            
+            // Closes the style parameter
+            $inlineStyles .= '"';
+        }
+        
+        // Full tag
+        $fullTag = '<' . $tag . $tagParams . $inlineStyles;
+        
+        // Checks the write mode
+        if( $onlyStart ) {
+            
+            // Only the start tag
+            $fullTag .= '>';
+            
+            // Registers the tag as open
+            $this->_startedTags[] = $tag;
+            
+        } elseif( $content || $tag === 'script' ) {
+            
+            // Adds the content and closes the tag
+            $fullTag .= '>' . $content . '</' . $tag . '>';
+            
+        } else {
+            
+            // Closes the tag
+            $fullTag .= ' />';
+        }
+        
+        // Returns the tag
+        return $fullTag;
+    }
+    
+    /**
+     * 
+     */
+    protected function _endTag()
+    {
+        // Checks for open tags
+        if( count( $this->_startedTags ) ) {
+            
+            // Gets and removes the last tag
+            $tag = array_pop( $this->_startedTags );
+            
+            // Returns the closing tag
+            return '</' . $tag . '>';
+        }
+        
+        // No open tag
+        return '';
+    }
+    
+    /**
+     * 
+     */
+    protected function _getFieldLabel( $table, $field )
+    {
+        // Checks if the table exists in the TCA
+        if( !isset( self::$_tca[ $table ] ) ) {
+            
+            // The table does not exist
+            return '';
+        }
+        
+        // Checks if the TCA is loaded
+        if( !isset( self::$_tca[ $table ][ 'columns' ] ) ) {
+            
+            // Loads the TCA
+            t3lib_div::loadTCA( $table );
+        }
+        
+        // Checks if the field exists in the TCA
+        if( !isset( self::$_tca[ $table ][ 'columns' ][ $field ] ) ) {
+            
+            // The field does not exist
+            return '';
+        }
+        
+        // Returns the label
+        return self::$_lang->sL( self::$_tca[ $table ][ 'columns' ][ $field ][ 'label' ] );
+    }
+    
+    /**
+     * 
+     */
     protected function _moduleContent()
     {
-        return 'CONTENT GOES HERE...';
+        // Checks the tables to see if something can be displayed
+        if( !$this->_getTables() ) {
+            
+            // No content
+            return self::$_lang->getLL( 'error.no-content' );
+        }
+        
+        // Checks the selection for the menu
+        switch( $this->MOD_SETTINGS[ 'function' ] ) {
+            
+            // Shows the registered users
+            default:
+                
+                return $this->_showRegisteredUsers();
+                break;
+        }
+    }
+    
+    /**
+     * 
+     */
+    protected function _getTables()
+    {
+        // Gets the frontend users
+        $users    = t3lib_BEfunc::getRecordsByField(
+            self::$_dbTables[ 'users' ],
+            'pid',
+            $this->id
+        );
+        
+        // Order by clause for the profiles
+        $orderBy  = ( $this->MOD_SETTINGS[ 'function' ] == 1 ) ? 'crdate DESC' : 'lastname,firstname';
+        
+        // Gets the user profiles
+        $profiles = t3lib_BEfunc::getRecordsByField(
+            self::$_dbTables[ 'profiles' ],
+            'pid',
+            $this->id,
+            '',
+            '',
+            $orderBy
+        );
+        
+        // Checks for users and profiles
+        if( is_array( $users ) && is_array( $profiles ) ) {
+            
+            // Process each user
+            foreach( $users as $user ) {
+                
+                // Stores the current user
+                $this->_users[ $user[ 'uid' ] ] = $user;
+            }
+            
+            // Process each profile
+            foreach( $profiles as $profile ) {
+                
+                // Stores the current profile
+                $this->_profiles[ $profile[ 'uid' ] ] = $profile;
+            }
+            
+            // Users and profiles were found
+            return true;
+        }
+        
+        // No user or profile
+        return false;
+    }
+    
+    /**
+     * 
+     */
+    protected function _showRegisteredUsers()
+    {
+        // Starts the table
+        $this->_content[] = $this->_tag(
+            'table',
+            '',
+            array(
+                'border'      => '0',
+                'width'       => '100%',
+                'align'       => 'center',
+                'cellpadding' => '2',
+                'cellspacing' => '1'
+            ),
+            array(
+                'background-color' => $this->doc->bgColor2
+            ),
+            true
+        );
+        
+        // Starts the table headers
+        $this->_content[] = $this->_tag( 'tr', '', array(), array(), true );
+        
+        // Styles for the table headers
+        $headerStyles     = array(
+            'font-weight' => 'bold'
+        );
+        
+        // Parameters for the table columns
+        $trParams         = array(
+            'align'  => 'left',
+            'valign' => 'middle'
+        );
+        
+        // Parameters for alternate rows
+        $alternateRows    = array(
+            array(
+                'params' => array(
+                    'onmouseover' => '',
+                    'onmouseout'  => '',
+                    'onclick'     => ''
+                ),
+                'styles' => array(
+                    'background-color' => $this->doc->bgColor4
+                )
+            ),
+            array(
+                'params' => array(
+                    'onmouseover' => '',
+                    'onmouseout'  => '',
+                    'onclick'     => ''
+                ),
+                'styles' => array(
+                    'background-color' => $this->doc->bgColor5
+                )
+            )
+        );
+        
+        // Adds the table headers
+        $this->_content[] = $this->_tag( 'td', '',                                                                   $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', '',                                                                   $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', $this->_getFieldLabel( self::$_dbTables[ 'profiles' ], 'lastname' ),  $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', $this->_getFieldLabel( self::$_dbTables[ 'profiles' ], 'firstname' ), $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', self::$_lang->getLL( 'headers.confirmed' ),                           $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', self::$_lang->getLL( 'headers.proof' ),                               $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', self::$_lang->getLL( 'headers.registration' ),                        $trParams, $headerStyles );
+        $this->_content[] = $this->_tag( 'td', $this->_getFieldLabel( self::$_dbTables[ 'profiles' ], 'birthdate' ), $trParams, $headerStyles );
+        
+        // Counter for rows
+        $rowCount         = 0;
+        
+        // Process each profile
+        foreach( $this->_profiles as $uid => $profile ) {
+            
+            // Confirmation state
+            $confirmed        = ( $profile[ 'confirm_token' ] )                           ? self::$_lang->getLL( 'no' )  : self::$_lang->getLL( 'yes' );
+            
+            // Proof documents state
+            $proof            = ( $profile[ 'age_proof' ] && $profile[ 'school_proof' ] ) ? self::$_lang->getLL( 'yes' ) : self::$_lang->getLL( 'no' );
+            
+            // Checkbox
+            $check            = $this->_tag(
+                'input',
+                '',
+                array(
+                    'type' => 'checkbox'
+                )
+            );
+            
+            // Starts the row
+            $this->_content[] = $this->_tag( 'tr', '', $alternateRows[ $rowCount ][ 'params' ], $alternateRows[ $rowCount ][ 'styles' ], true );
+            
+            // Adds the checkbox
+            $this->_content[] = $this->_tag( 'td', $check, $trParams );
+            
+            // Adds the icons
+            $this->_content[] = $this->_tag( 'td', $this->_api->be_getRecordCSMIcon( self::$_dbTables[ 'profiles' ], $profile, self::$_backPath ), $trParams );
+            
+            // Adds the full name
+            $this->_content[] = $this->_tag( 'td', $profile[ 'lastname' ], $trParams );
+            $this->_content[] = $this->_tag( 'td', $profile[ 'firstname' ], $trParams );
+            
+            // Adds the confirmation state
+            $this->_content[] = $this->_tag( 'td', $confirmed, $trParams );
+            
+            // Adds the proof documents state
+            $this->_content[] = $this->_tag( 'td', $proof, $trParams );
+            
+            // Adds the registration date
+            $this->_content[] = $this->_tag( 'td', date( self::$_dateFormat, $profile[ 'crdate' ] ), $trParams );
+            
+            // Adds the user birth date
+            $this->_content[] = $this->_tag( 'td', date( self::$_dateFormat, $profile[ 'birthdate' ] ), $trParams );
+            
+            // Ends the row
+            $this->_content[] = $this->_endTag();
+            
+            // Changes the row counter
+            $rowCount         = ( $rowCount === 0 ) ? 1 : 0;
+        }
+        
+        // Ends the table headers
+        $this->_content[] = $this->_endTag();
+        
+        // Ends the table
+        $this->_content[] = $this->_endTag();
     }
     
     /**
@@ -243,7 +582,7 @@ class tx_adlercontest_module1 extends t3lib_SCbase
     public function main()
     {
         // Gets the page informations
-        $this->_pageInfos         =  t3lib_BEfunc::readPageAccess(
+        $this->_pageInfos =  t3lib_BEfunc::readPageAccess(
             $this->id,
             $this->perms_clause
         );
@@ -252,17 +591,39 @@ class tx_adlercontest_module1 extends t3lib_SCbase
         if( ( $this->id && is_array( $this->_pageInfos ) ) || ( self::$_beUser->user[ 'admin' ] && !$this->id ) ) {
             
             // Creates the main form tag
-            $this->doc->form = '<form action="" method="post" enctype="'
-                             . self::$_typo3ConfVars[ 'SYS' ][ 'form_enctype' ]
-                             . '" id="'
-                             . __CLASS__
-                             . '" name="'
-                             . __CLASS__
-                             . '">';
+            $this->doc->form = $this->_tag(
+                'form',
+                '',
+                array(
+                    'action'  => '',
+                    'method'  => 'post',
+                    'enctype' => self::$_typo3ConfVars[ 'SYS' ][ 'form_enctype' ],
+                    'id'      => __CLASS__,
+                    'name'    => __CLASS__
+                ),
+                array(),
+                true
+            );
             
             // JavaScript files
-            $this->doc->JScode   = '<script src="mod.js" type="text/javascript" charset="utf-8"></script>';
-            $this->doc->postCode = '<script src="mod-postcode.js" type="text/javascript" charset="utf-8"></script>';
+            $this->doc->JScode   = $this->_tag(
+                'script',
+                '',
+                array(
+                    'src'     => 'mod.js',
+                    'type'    => 'text/javascript',
+                    'charset' => 'utf-8',
+                )
+            );
+            $this->doc->postCode = $this->_tag(
+                'script',
+                '',
+                array(
+                    'src'     => 'mod-postcode.js',
+                    'type'    => 'text/javascript',
+                    'charset' => 'utf-8',
+                )
+            );
             
             // Initializes the context sensitive menu
             $this->_api->be_initCsm();
@@ -289,7 +650,9 @@ class tx_adlercontest_module1 extends t3lib_SCbase
             
             // Creates the full header section
             $headerSection = $this->doc->funcMenu(
-                $header . '<br />' . $path,
+                $header
+              . $this->_tag( 'br' )
+              . $path,
                 $funcMenu
             );
             
@@ -312,13 +675,14 @@ class tx_adlercontest_module1 extends t3lib_SCbase
                 );
                 
                 // Adds the shortcut
-                $this->_content[] = $this->doc->spacer( 20 ) . $this->doc->section( '', $shortcut );;
+                $this->_content[] = $this->doc->spacer( 20 )
+                                  . $this->doc->section( '', $shortcut );
             }
             
         } else {
             
             // No access
-            $this->_content[] = self::$_lang->getLL( 'noaccess' );
+            $this->_content[] = self::$_lang->getLL( 'error.no-access' );
         }
     }
     
@@ -330,7 +694,8 @@ class tx_adlercontest_module1 extends t3lib_SCbase
         // Array with the menu items
         $this->MOD_MENU = array (
             'function' => array (
-                '1' => self::$_lang->getLL( 'menu.func.1' )
+                '1' => self::$_lang->getLL( 'menu.func.1' ),
+                '2' => self::$_lang->getLL( 'menu.func.2' )
             )
         );
         
@@ -340,7 +705,7 @@ class tx_adlercontest_module1 extends t3lib_SCbase
 }
 
 // XClass inclusion
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/adler_contest/mod1/index.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/adler_contest/mod1/index.php']) {
     include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/adler_contest/mod1/index.php']);
 }
 
