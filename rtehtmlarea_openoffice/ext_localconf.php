fb25665f8bca6c0411e1565fe785efcbe3672f5f
macmade <macmade>
2009-01-27 05:50:25 +0000

    Code cleaning.

--- rtehtmlarea_openoffice/ext_localconf.php
+++ rtehtmlarea_openoffice/ext_localconf.php
@@ -1,18 +1,20 @@
 <?php
-	if (!defined ('TYPO3_MODE')) {
-		die ('Access denied.');
-	}
-	
-	// Get extension configuration
-	$extConf = unserialize($_EXTCONF);
-	
-	// Check if skin should be used by default
-	if (array_key_exists('useAsDefaultSkin',$extConf) && $extConf['useAsDefaultSkin']) {
-		
-		// Add skin
-		t3lib_extMgm::addPageTSConfig('
-			RTE.default.skin = EXT:' . $_EXTKEY . '/ooo/htmlarea.css
-			RTE.default.FE.skin = EXT:' . $_EXTKEY . '/ooo/htmlarea.css
-		');
-	}
+
+if( !defined( 'TYPO3_MODE' ) ) {
+    die( 'Access denied.' );
+}
+
+// Get extension configuration
+$extConf = unserialize( $_EXTCONF );
+
+// Check if skin should be used by default
+if( isset( $extConf[ 'useAsDefaultSkin' ] ) ) {
+    
+    // Add skin
+    t3lib_extMgm::addPageTSConfig( '
+        RTE.default.skin = EXT:'    . $_EXTKEY . '/ooo/htmlarea.css
+        RTE.default.FE.skin = EXT:' . $_EXTKEY . '/ooo/htmlarea.css
+    ' );
+}
+
 ?>
