33dc9d19bd21b8e4fd53c48de7a90f51849a4d84
macmade <macmade>
2009-02-02 05:47:56 +0000

    Initial import of the 'ooptest' extension.

---
+++ ooptest/ext_emconf.php
@@ -0,0 +1,47 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "ooptest"
+#
+# Auto generated 01-02-2009 14:37
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'OOP Test',
+	'description' => 'A test extension for the OOP framework.',
+	'category' => 'example',
+	'author' => 'Jean-David Gadina',
+	'author_email' => 'macmade@eosgarden.com',
+	'shy' => '',
+	'dependencies' => 'oop',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => 'mod1',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => 'macmade.net',
+	'version' => '0.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'oop' => '0.0.0-0.0.0'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:12:"ext_icon.gif";s:4:"c839";s:17:"ext_localconf.php";s:4:"8af1";s:37:"classes/class.tx_oop_classmanager.php";s:4:"e4db";s:38:"classes/class.tx_oop_exceptionbase.php";s:4:"477c";s:29:"classes/class.tx_oop_lang.php";s:4:"8123";s:38:"classes/class.tx_oop_langexception.php";s:4:"9890";s:35:"classes/class.tx_oop_modulebase.php";s:4:"4078";s:43:"classes/class.tx_oop_singletonexception.php";s:4:"d835";}',
+	'suggests' => array(
+	),
+);
+
+?>
---
+++ ooptest/mod1/conf.php
@@ -0,0 +1,7 @@
+<?php
+
+$MCONF['name']                          = 'web_txooptestM1';
+$MCONF['script']                        = '_DISPATCH';
+$MCONF['access']                        = 'user,group';
+$MLANG['default']['tabs_images']['tab'] = 'moduleicon.gif';
+$MLANG['default']['ll_ref']             = 'LLL:EXT:ooptest/lang/mod1.xml';
