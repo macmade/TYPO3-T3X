3826277185f51a0542c4048e853cbd187a2a4692
macmade <macmade>
2009-02-03 13:24:19 +0000

    Added the lang getter exception class.

---
+++ oop/classes/tx/oop/Lang/Getter/Exception.class.php
@@ -0,0 +1,45 @@
+<?php
+/***************************************************************
+ * Copyright notice
+ * 
+ * (c) 2009 Jean-David Gadina (macmade@eosgarden.com)
+ * All rights reserved
+ * 
+ * This script is part of the TYPO3 project. The TYPO3 project is 
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * The GNU General Public License can be found at
+ * http://www.gnu.org/copyleft/gpl.html.
+ * 
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+# $Id$
+
+// DEBUG ONLY - Sets the error reporting level to the highest possible value
+#error_reporting( E_ALL | E_STRICT );
+
+/**
+ * Exception class for the tx_oop_Lang_Getter class
+ *
+ * @author      Jean-David Gadina <macmade@eosgarden.com>
+ * @version     1.0
+ * @package     TYPO3
+ * @subpackage  oop
+ */
+final class tx_oop_Lang_Getter_Exception extends tx_oop_Core_Exception_Base
+{
+    /**
+     * Error codes for the exceptions
+     */
+    const EXCEPTION_NO_LANG_FILE = 0x01;
+    const EXCEPTION_BAD_XML      = 0x02;
+}
